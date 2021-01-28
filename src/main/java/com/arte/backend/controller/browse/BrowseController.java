773059ad9bc7c1cc8c10409d34c9ed44c931d25d
BalazsMH <horvathb91@gmail.com>
2021-01-28 13:05:11 +0100

    Add server-side museum api call functionality.

--- src/main/java/com/arte/backend/controller/browse/BrowseController.java
+++ src/main/java/com/arte/backend/controller/browse/BrowseController.java
@@ -1,6 +1,7 @@
 package com.arte.backend.controller.browse;
 
 import com.arte.backend.service.browse.BrowseService;
+import com.arte.backend.service.browse.MuseumApiDataProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class BrowseController {
     @Autowired
     BrowseService browseService;
+    @Autowired
+    MuseumApiDataProviderService museumApiDataProviderService;
 
 
     @GetMapping("/browse")
     public String renderBrowse() {
         return browseService.browse();
+    }
+
+    @GetMapping("/api/getArtData")
+    public String returnArtData() {
+        return museumApiDataProviderService.getArtData();
     }
 }
--- src/main/java/com/arte/backend/service/browse/MuseumApiDataProviderService.java
+++ src/main/java/com/arte/backend/service/browse/MuseumApiDataProviderService.java
@@ -1,9 +1,29 @@
 package com.arte.backend.service.browse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Service
 public class MuseumApiDataProviderService {
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    private String apiURL = "https://www.rijksmuseum.nl/api/en/collection?key=Gz1ZRsyI&format=json";
+
+    public String getArtData() {
+        String response = webClientBuilder.build()
+                .get()
+                .uri(apiURL)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println("Api call finished");
+        return response;
+    }
 
 }
