28848080a5c204f2694c850396350636c31b229a
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-02-05 16:03:19 +0100

    Move getQuiz to service from controller

--- src/main/java/com/arte/backend/controller/quiz/QuizController.java
+++ src/main/java/com/arte/backend/controller/quiz/QuizController.java
@@ -1,13 +1,7 @@
 package com.arte.backend.controller.quiz;
 
-import com.arte.backend.model.apiresponse.ArtObjectsList;
-import com.arte.backend.model.quiz.QuizModel;
-import com.arte.backend.model.quiz.QuizType;
-import com.arte.backend.service.quiz.QuizDataProviderService;
-import com.arte.backend.util.quiz.PageNumberGenerator;
-import com.arte.backend.util.quiz.QuizGenerator;
+import com.arte.backend.service.quiz.QuizService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,25 +9,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/quiz")
 @AllArgsConstructor
 public class QuizController {
-    private final String resultsPerPage = "20";
-    private final String type = "painting";
-
-    QuizDataProviderService quizDataProviderService;
-    PageNumberGenerator pageNumberGenerator;
+    private QuizService quizService;
 
     @CrossOrigin
     @PostMapping
     public String getQuiz(@RequestBody String quizType) throws JsonProcessingException {
-        String pageNumber = pageNumberGenerator.generateRandomPageNumber();
-        String artObject = quizDataProviderService.getDataForQuiz(pageNumber, resultsPerPage, type, true);
-        ObjectMapper mapper = new ObjectMapper();
-
-        ArtObjectsList artObjectsList = mapper.readValue(artObject, ArtObjectsList.class);
-        QuizType type = mapper.readValue(quizType, QuizType.class);
-
-        QuizGenerator quizGenerator = new QuizGenerator(artObjectsList, type.getQuizType());
-        QuizModel quiz = quizGenerator.generateQuiz(new QuizModel());
-        String quizJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(quiz);
-        return quizJson;
+        return quizService.getQuiz(quizType);
     }
 }
