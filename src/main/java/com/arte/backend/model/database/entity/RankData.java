f63542352962418e3f1e658df4157dd5f0ab4fde
BalazsMH <horvathb91@gmail.com>
2021-02-07 16:06:36 +0100

    Implement RankData model

--- src/main/java/com/arte/backend/model/database/entity/RankData.java
+++ src/main/java/com/arte/backend/model/database/entity/RankData.java
@@ -1,2 +1,26 @@
-package com.arte.backend.model.database.entity;public class RankData {
+package com.arte.backend.model.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RankData {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Enumerated
+    private RankName name;
+    private String badgeUrl;
+    private long xpLimit;
+    @OneToMany(mappedBy = "rank", cascade = CascadeType.PERSIST)
+    private Set<UserStatistics> userStatisticsSet;
 }
