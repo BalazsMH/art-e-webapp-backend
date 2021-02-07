1ef819b5bc8f5e42ce879da27bdece4fcf206379
BalazsMH <horvathb91@gmail.com>
2021-02-07 15:14:43 +0100

    Implement UserData model

--- src/main/java/com/arte/backend/model/database/entity/UserData.java
+++ src/main/java/com/arte/backend/model/database/entity/UserData.java
@@ -1,2 +1,31 @@
-package com.arte.backend.model.database.entity;public class UserData {
+package com.arte.backend.model.database.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserData {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String password;
+    private LocalDate birthDate;
+    private LocalDate registrationDate;
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.PERSIST)
+    @Singular
+    @EqualsAndHashCode.Exclude
+    private Set<FavoriteFolder> favoriteFolders;
+
 }
