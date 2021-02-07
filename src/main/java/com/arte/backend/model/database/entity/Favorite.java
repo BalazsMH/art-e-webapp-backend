cf158554e7e66ff86afc7d9498c0fd3741fb2cec
BalazsMH <horvathb91@gmail.com>
2021-02-07 15:45:21 +0100

    Implement Favorite model

--- src/main/java/com/arte/backend/model/database/entity/Favorite.java
+++ src/main/java/com/arte/backend/model/database/entity/Favorite.java
@@ -1,2 +1,22 @@
-package com.arte.backend.model.database.entity;public class Favorite {
+package com.arte.backend.model.database.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Favorite {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String objectNumber;
+    @ManyToMany(mappedBy = "favorites", cascade = CascadeType.PERSIST)
+    @Singular
+    @EqualsAndHashCode.Exclude
+    private Set<FavoriteFolder> favoriteFolders;
 }
