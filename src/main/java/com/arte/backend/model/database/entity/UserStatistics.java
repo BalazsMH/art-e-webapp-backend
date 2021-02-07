12865860e16e50bd8f443ee532e286afce6c566e
BalazsMH <horvathb91@gmail.com>
2021-02-07 16:08:26 +0100

    Implement UserStatistics model

--- src/main/java/com/arte/backend/model/database/entity/UserStatistics.java
+++ src/main/java/com/arte/backend/model/database/entity/UserStatistics.java
@@ -1,2 +1,30 @@
-package com.arte.backend.model.database.entity;public class UserStatistics {
+package com.arte.backend.model.database.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserStatistics {
+    @Id
+    @GeneratedValue
+    private long id;
+    private long actualXp;
+    private int allAnswers;
+    private int correctAnswers;
+    private int winStreak;
+    private int dailyStreak;
+    private int dailyRemainingXp;
+    @ManyToOne
+    private RankData rank;
+    @OneToOne(mappedBy = "userStatistics", cascade = CascadeType.PERSIST)
+    @EqualsAndHashCode.Exclude
+    private UserData user;
+
 }
