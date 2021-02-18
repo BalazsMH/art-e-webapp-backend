40278a4e278ed79eb691e15f76ea999e36830d84
BalazsMH <horvathb91@gmail.com>
2021-02-18 15:57:51 +0100

    Add findbyusername method to the UserRepository interface

--- src/main/java/com/arte/backend/repository/UserRepository.java
+++ src/main/java/com/arte/backend/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.arte.backend.repository;
 import com.arte.backend.model.database.entity.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserData, Long> {
-    
+
+    Optional<UserData> findByUserName(String username);
+
 }
