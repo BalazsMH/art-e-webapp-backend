527507eafc3ab545362c39e49f269871b7cf9159
BalazsMH <horvathb91@gmail.com>
2021-02-19 00:01:37 +0100

    Add duplicate email checking before user registration

--- src/main/java/com/arte/backend/controller/registration/RegistrationController.java
+++ src/main/java/com/arte/backend/controller/registration/RegistrationController.java
@@ -1,14 +1,14 @@
 package com.arte.backend.controller.registration;
 
-import com.arte.backend.model.database.entity.UserData;
-import com.arte.backend.model.database.entity.UserRole;
 import com.arte.backend.service.registration.RegistrationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Collections;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
@@ -17,16 +17,16 @@ public class RegistrationController {
     RegistrationService registrationService;
 
     @CrossOrigin(origins = "http://localhost:3000")
-    @PostMapping("/register")
-    public String register(@RequestParam( name = "userName") String userName,
-                           @RequestParam( name = "firstName") String firstName,
-                           @RequestParam( name = "lastName") String lastName,
-                           @RequestParam( name = "email") String email,
-                           @RequestParam( name = "password") String password,
-                           @RequestParam( name = "birthDate") String birthDate) {
+    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> register(@RequestParam( name = "userName") String userName,
+                                   @RequestParam( name = "firstName") String firstName,
+                                   @RequestParam( name = "lastName") String lastName,
+                                   @RequestParam( name = "email") String email,
+                                   @RequestParam( name = "password") String password,
+                                   @RequestParam( name = "birthDate") String birthDate) {
 
-        registrationService.registerUser(userName, firstName, lastName, email, password, birthDate);
+        JSONObject responseJson = registrationService.registerUser(userName, firstName, lastName, email, password, birthDate);
 
-        return null;
+        return ResponseEntity.ok().body(responseJson.toString());
     }
 }
--- src/main/java/com/arte/backend/service/registration/RegistrationService.java
+++ src/main/java/com/arte/backend/service/registration/RegistrationService.java
@@ -4,6 +4,7 @@ import com.arte.backend.model.database.entity.UserData;
 import com.arte.backend.model.database.entity.UserRole;
 import com.arte.backend.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -14,9 +15,23 @@ import java.util.Collections;
 public class RegistrationService {
     UserRepository userRepository;
 
-    public void registerUser(String userName, String firstName, String lastName,
-                             String email, String password, String birthDate) {
+    public JSONObject registerUser(String userName, String firstName, String lastName,
+                                   String email, String password, String birthDate) {
+
         System.out.println(userName + firstName + lastName + email + password + birthDate);
+
+        boolean emailAlreadyExists = userRepository.existsByEmail(email);
+
+        JSONObject response = new JSONObject();
+        response.put("emailNotAvailable", emailAlreadyExists);
+
+        if (emailAlreadyExists) {
+            System.out.println("email already in use");
+            return response;
+        } else {
+            System.out.println("email ok");
+        }
+
         UserData user = UserData.builder()
                 .userName(userName)
                 .firstName(firstName)
@@ -30,6 +45,7 @@ public class RegistrationService {
         //TODO:validate data and send response
         System.out.println(user.toString());
         userRepository.save(user);
-        System.out.println("save use");
+        System.out.println("save user");
+        return response;
     }
 }
