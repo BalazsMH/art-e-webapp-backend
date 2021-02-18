02b07cb10c956cd704e2efa9b636d579ac79ee91
BalazsMH <horvathb91@gmail.com>
2021-02-18 22:05:38 +0100

    Add Registration Service and implement registerUser method

--- src/main/java/com/arte/backend/service/registration/RegistrationService.java
+++ src/main/java/com/arte/backend/service/registration/RegistrationService.java
@@ -1,2 +1,30 @@
-package com.arte.backend.service.registration;public class RegistrationService {
+package com.arte.backend.service.registration;
+
+import com.arte.backend.model.database.entity.UserData;
+import com.arte.backend.model.database.entity.UserRole;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    public void registerUser(String userName, String firstName, String lastName,
+                             String email, String password, String birthDate) {
+        System.out.println(userName + firstName + lastName + email + password + birthDate);
+        UserData user = UserData.builder()
+                .userName(userName)
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(email)
+                .password(password)
+                .registrationDate(LocalDate.now())
+                .birthDate(LocalDate.parse(birthDate))
+                .roles(Collections.singletonList(UserRole.USER))
+                .build();
+        System.out.println(user.toString());
+    }
 }
