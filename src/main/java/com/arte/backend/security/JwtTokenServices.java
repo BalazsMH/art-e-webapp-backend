4e38b17d39a5cc5d236ceddcf8ae81989706af44
BalazsMH <horvathb91@gmail.com>
2021-02-18 14:22:56 +0100

    Implement JwtTokenServices init method

--- src/main/java/com/arte/backend/security/JwtTokenServices.java
+++ src/main/java/com/arte/backend/security/JwtTokenServices.java
@@ -3,14 +3,22 @@ package com.arte.backend.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+
 @Component
 public class JwtTokenServices {
 
     @Value("${security.jwt.token.secret-key:secret}")
-    private String secretKey = "secret";
+    private String secretKey = "arte5864";
 
     @Value("${security.jwt.token.expire-length:3600000}")
     private long validityInMilliseconds = 3600000;
 
     private final String rolesFieldName = "roles";
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString((secretKey.getBytes()));
+    }
 }
