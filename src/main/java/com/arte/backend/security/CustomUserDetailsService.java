496062f2d36557529208e41a87442ec45ee693b3
BalazsMH <horvathb91@gmail.com>
2021-02-18 16:30:01 +0100

    Add and implement CustomUserDetailsService loadUserByUsername method

--- src/main/java/com/arte/backend/security/CustomUserDetailsService.java
+++ src/main/java/com/arte/backend/security/CustomUserDetailsService.java
@@ -1,2 +1,28 @@
-package com.arte.backend.security;public class CustomUserDetailsService {
+package com.arte.backend.security;
+
+import com.arte.backend.model.database.entity.UserData;
+import com.arte.backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserData user = userRepository.findByUserName(username)
+                .orElseThrow(()->new UsernameNotFoundException("Username: " + username + "not found"));
+        return new User(user.getUserName(),
+                        user.getPassword(),
+                        user.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+    }
 }
