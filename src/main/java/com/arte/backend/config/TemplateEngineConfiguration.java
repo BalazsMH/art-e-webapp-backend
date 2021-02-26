c97389857ac8a18fa460c81d1b2c5c05ffde13ee
BalazsMH <horvathb91@gmail.com>
2021-02-26 03:19:02 +0100

    Add TemplateEngineConfiguration class for thymeleaf integration

--- src/main/java/com/arte/backend/config/TemplateEngineConfiguration.java
+++ src/main/java/com/arte/backend/config/TemplateEngineConfiguration.java
@@ -1,2 +1,30 @@
-package com.arte.backend.config;public class TemplateEngineConfiguration {
+package com.arte.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class TemplateEngineConfiguration {
+    @Bean
+    public ITemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        return templateResolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngineProvider() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(this.templateResolver());
+
+        return templateEngine;
+    }
+
 }
