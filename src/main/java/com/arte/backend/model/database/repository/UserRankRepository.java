56c9599e12eb7896275bd53630335887f88ef08c
BalazsMH <horvathb91@gmail.com>
2021-03-12 17:10:24 +0100

    Add method for finding all rank names

--- src/main/java/com/arte/backend/model/database/repository/UserRankRepository.java
+++ src/main/java/com/arte/backend/model/database/repository/UserRankRepository.java
@@ -3,8 +3,10 @@ package com.arte.backend.model.database.repository;
 import com.arte.backend.model.database.entity.RankData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRankRepository extends JpaRepository<RankData, Long> {
     Optional<RankData> findById(Long id);
+    List<RankData> findBy();
 }
