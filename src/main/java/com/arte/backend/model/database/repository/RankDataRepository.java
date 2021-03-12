476369afc702ec01bd516e576a58a93167cbd5cb
BalazsMH <horvathb91@gmail.com>
2021-03-12 17:08:18 +0100

    Add endpoint for get-available-ranks

--- src/main/java/com/arte/backend/model/database/repository/RankDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.arte.backend.model.database.repository;
-
-import com.arte.backend.model.database.entity.RankData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RankDataRepository extends JpaRepository<RankData, Long> {
-    
-}
