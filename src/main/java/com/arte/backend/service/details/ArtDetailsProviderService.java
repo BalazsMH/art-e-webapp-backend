9ad334d9eb0f787e47532a8277a7cfd83f8271bb
BalazsMH <horvathb91@gmail.com>
2021-03-17 17:15:02 +0100

    Fix incorrect server response when the museum api responds with an error.

--- src/main/java/com/arte/backend/service/details/ArtDetailsProviderService.java
+++ src/main/java/com/arte/backend/service/details/ArtDetailsProviderService.java
@@ -1,8 +1,12 @@
 package com.arte.backend.service.details;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ArtDetailsProviderService {
@@ -33,6 +37,7 @@ public class ArtDetailsProviderService {
                 )
                 .retrieve()
                 .bodyToMono(String.class)
+                .onErrorResume(WebClientResponseException.class, e -> Mono.empty())
                 .block();
         return response;
     }
